2da050cfc851a54ed8450a617d8d2e9bf84b7581
Kevin Quintino <42918428+kevinnicolasbq@users.noreply.github.com>
2020-03-14 13:57:56 -0300

    Delete python.py

--- python.py
+++
@@ -1,9 +0,0 @@
-tamanhoArquivo = float(input("Digite o tamanho do arquivo(em MB): "))
-velocidadeInternet = float(input("Digite a velocidade de download ou upload (em Mbps): "))
-
-velocidadeInternet *= 1024 / 8000
-tempoEstimadoInteiro = (tamanhoArquivo / velocidadeInternet) // 60
-tempoEstimadoDecimal = (tamanhoArquivo / velocidadeInternet) % 60
-
-
-print('O tempo estimado para o download (ou upload) é de {:.0f} minutos e {:.0f} segundos.'.format(tempoEstimadoInteiro, tempoEstimadoDecimal))
